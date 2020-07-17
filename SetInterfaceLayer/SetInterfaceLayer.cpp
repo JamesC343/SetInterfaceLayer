cfb10ca9cc791a00d11d1c1d768d86f1da4368c9
james <james@JAMES-PC>
2020-07-17 15:58:46 +1200

    Removed the need for Vector pointer within the Table.column Vector Adjusted 'InnerJoin' code to be more readable

--- SetInterfaceLayer/SetInterfaceLayer.cpp
+++ SetInterfaceLayer/SetInterfaceLayer.cpp
@@ -6,7 +6,7 @@
 
 struct Table
 {
-    std::vector<std::vector<int>*> columns;
+    std::vector<std::vector<int>> columns;
 };
 
 struct SelectList
@@ -34,12 +34,12 @@ void Print(Table table)
     int columnCount = table.columns.size();
     if (columnCount > 0)
     {
-        int rowCount = table.columns.at(0)->size();
+        int rowCount = table.columns.at(0).size();
         for (int i = 0; i < rowCount; i++)
         {
             for (int j = 0; j < columnCount; j++)
             {
-                int value = table.columns.at(j)->at(i);
+                int value = table.columns.at(j).at(i);
                 std::cout << value << "\t";
             }
             std::cout << std::endl;
@@ -51,36 +51,37 @@ void Print(Table table)
 //Join limited to 1 entry for now
 Table InnerJoin(Table tableA, Table tableB, std::vector<SelectList> select, std::vector<JoinList> join)
 {
-    Table newTable;
-    for (int i = 0; i < select.size(); i++)
-    {
-        newTable.columns.push_back(new std::vector<int>);
-    }
+    Table newTable;//select.size() determines the number of columns in the new table
+    newTable.columns.resize(select.size());
 
     //Both tables have columns
     if (tableA.columns.size() > 0 && tableB.columns.size() > 0)
     {
-        //Both tables have rows
-        int tableARowCount = tableA.columns.at(0)->size();
-        int tableBRowCount = tableB.columns.at(0)->size();
+        //Both tables have rows     //In the future there will be a different way to manage the record counts in a table
+        int tableARowCount = tableA.columns.at(0).size();
+        int tableBRowCount = tableB.columns.at(0).size();
         if (tableARowCount > 0 && tableBRowCount > 0)
         {
-            for (int i = 0; i < tableARowCount; i++)
+            for (int ARow = 0; ARow < tableARowCount; ARow++)
             {
-                for (int j = 0; j < tableBRowCount; j++)
-                {
-                    int tableAValue = tableA.columns.at(join.at(0).tableAColumn)->at(i);
-                    int tableBValue = tableB.columns.at(join.at(0).tableBColumn)->at(j);
-                    if (tableAValue == tableBValue)
+                for (int BRow = 0; BRow < tableBRowCount; BRow++)
+                {                                                       //Hardcoded join at 0 index
+                    std::vector<int>& tableAJoinColumn = tableA.columns.at(join.at(0).tableAColumn);
+                    std::vector<int>& tableBJoinColumn = tableB.columns.at(join.at(0).tableBColumn);
+                    int tableAJoinValue = tableAJoinColumn.at(ARow);
+                    int tableBJoinValue = tableBJoinColumn.at(BRow);
+
+                    if (tableAJoinValue == tableBJoinValue)
                     {
-                        for (int k = 0; k < select.size(); k++)
+                        for (int selectID = 0; selectID < select.size(); selectID++)
                         {
-                            int value = 0;
-                            if (select.at(k).table == 0)
-                                value = tableA.columns.at(select.at(k).column)->at(i);
-                            else if (select.at(k).table == 1)
-                                value = tableB.columns.at(select.at(k).column)->at(j);
-                            newTable.columns.at(k)->push_back(value);
+                            int tableID = select.at(selectID).table;
+                            int columnID = select.at(selectID).column;
+                            int rowID = (tableID == 0 ? ARow : BRow);
+
+                            int value = (tableID == 0 ? tableA : tableB).columns.at(columnID).at(rowID);
+
+                            newTable.columns.at(selectID).push_back(value);
                         }
                     }
                 }
@@ -96,11 +97,11 @@ int main()
 {
     std::vector<int> tableAcolumnA({ 1,2 });
     std::vector<int> tableAcolumnB({ 3,4 });
-    Table tableA({ { &tableAcolumnA ,&tableAcolumnB } });
+    Table tableA({ { tableAcolumnA ,tableAcolumnB } });
 
     std::vector<int> tableBcolumnA({ 2,1 });
     std::vector<int> tableBcolumnB({ 5,6 });
-    Table tableB({ { &tableBcolumnA ,&tableBcolumnB } });
+    Table tableB({ { tableBcolumnA ,tableBcolumnB } });
 
     Table tableC = InnerJoin(tableA, tableB, { {0,0},{0,1},{1,1} }, { {0,0} });
 
